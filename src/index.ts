6bceec6765ed459736b7e021e50d01ffa2c0a455
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-06-11 10:22:41 +0200

    "Updated CSS styles, added new HTML elements, and refactored TypeScript code to include new functions and data structures."

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,9 @@
 // all TypeScript weakness flags.
 // : number
 import "../index.css";
+import { showReviewTotal, populateUser } from "./utils";
 
-const returningUserDisplay = document.querySelector("#returning-user");
-const userNameDisplay = document.querySelector("#user");
-const reviewTotalDisplay = document.querySelector("#reviews");
+let isOpen: boolean;
 
 const reviews: {
   name: string;
@@ -34,19 +33,7 @@ const reviews: {
   },
 ];
 
-function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
-  const iconDisplay = isLoyalty ? "⭐" : "";
-  reviewTotalDisplay.innerHTML =
-    "review total " +
-    value.toString() +
-    "| last reviewed by " +
-    reviewer +
-    " " +
-    iconDisplay;
-}
-
-showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
-
+// User
 const you: {
   firstName: string;
   lastName: string;
@@ -61,11 +48,62 @@ const you: {
   stayedAt: ["florida-home", "oman-flat", "tokyo-bungalow"],
 };
 
-function populateUser(isReturning: boolean, firstName: string) {
-  if (isReturning) {
-    returningUserDisplay.innerHTML = "back";
-  }
-  userNameDisplay.innerHTML = firstName;
-}
+// Array of Properties
+const properties: {
+  image: string;
+  title: string;
+  price: number;
+  location: {
+    firstLine: string;
+    city: string;
+    code: number;
+    country: string;
+  };
+  contact: string;
+  isAvailable: boolean;
+}[] = [
+  {
+    image: "./images/colombia-property.jpg",
+    title: "Colombian Shack",
+    price: 45,
+    location: {
+      firstLine: "shack 37",
+      city: "Bogota",
+      code: 45632,
+      country: "Colombia",
+    },
+    contact: "marywinkle@gmail.com",
+    isAvailable: true,
+  },
+  {
+    image: "images/poland-property.jpg",
+    title: "Polish Cottage",
+    price: 34,
+    location: {
+      firstLine: "no 23",
+      city: "Gdansk",
+      code: 343903,
+      country: "Poland",
+    },
+    contact: "garydavis@hotmail.com",
+    isAvailable: false,
+  },
+  {
+    image: "images/london-property.jpg",
+    title: "London Flat",
+    price: 23,
+    location: {
+      firstLine: "flat 15",
+      city: "London",
+      code: 35433,
+      country: "United Kingdom",
+    },
+    contact: "andyluger@aol.com",
+    isAvailable: true,
+  },
+];
+
+// Functions
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
 
 populateUser(you.isReturning, you.firstName);
